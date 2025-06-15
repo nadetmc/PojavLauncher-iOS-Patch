ffd6f93fca6c6c201687cf483e3fdc33580cbb02
congcq <aiskai2007@gmail.com>
2025-06-15 15:54:41 +0700

    Feat(renderer): re-added old gl4es (not tested)

--- Natives/LauncherPreferences.m
+++ Natives/LauncherPreferences.m
@@ -137,6 +137,7 @@ NSArray* getRendererKeys(BOOL containsDefault) {
     NSMutableArray *array = @[
         @"auto",
         @ RENDERER_NAME_GL4ES,
+        @ RENDERER_NAME_GL4ES_114,
         @ RENDERER_NAME_MTL_ANGLE,
         @ RENDERER_NAME_MOBILEGLUES,
         @ RENDERER_NAME_VK_ZINK
@@ -154,6 +155,7 @@ NSArray* getRendererNames(BOOL containsDefault) {
 
     array = @[
         localize(@"preference.title.renderer.debug.auto", nil),
+        localize(@"preference.title.renderer.debug.gl4es_114", nil),
         localize(@"preference.title.renderer.debug.gl4es", nil),
         localize(@"preference.title.renderer.debug.angle", nil),
         localize(@"preference.title.renderer.debug.mobileglues", nil),
